7e3649bc948526279a0817895700dcf40d2a0343
luanmvcosta0 <luanmvcosta@gmail.com>
2026-01-19 14:36:27 -0300

    refactor: removendo função hello de teste inicial

--- musica.py
+++ musica.py
@@ -3,11 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/inicio")
-def hello():
-    return "Ola mundo"
-
-
 @app.route("/musicas")
 def listar_musicas():
     return render_template("lista_musicas.html")
