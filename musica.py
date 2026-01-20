7638d8dacbe5c975c92c19961a6073604dcc271a
Luan Costa <104772288+luanmvcosta0@users.noreply.github.com>
2026-01-20 09:04:11 -0300

    feature/pagina-lista
    * feat: atualizar lista de músicas e renderizar dinamicamente no template
    
    * feat: implementar classe Musica e atualizar template para exibir detalhes das músicas

--- musica.py
+++ musica.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
+
+class Musica:
+    def __init__(self, nome, cantorBanda, genero):
+        self.nome = nome
+        self.cantorBanda = cantorBanda
+        self.genero = genero
+
+
 app = Flask(__name__)
 
 
 @app.route("/musicas")
 def listar_musicas():
-    return render_template("lista_musicas.html")
+
+    musica01 = Musica("Loira de Medicina", "G.A", "Trap")
+    musica02 = Musica("Papai Banca", "MC Ryan SP", "Funk")
+    musica03 = Musica("Camisa 10", "Turma do Pagode", "Pagode")
+
+    lista = [musica01, musica02, musica03]
+
+    return render_template("lista_musicas.html", musicas=lista)
 
 
-app.run()
+app.run(debug=True)
